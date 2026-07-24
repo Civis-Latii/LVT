eb0955154cdd17ff16a5cf45eb9198284e7c09bb
Latium <254908945+Civis-Latii@users.noreply.github.com>
2026-07-24 19:12:14 +0100

    corrected explanation

--- core_lexicon/__init__.py
+++ core_lexicon/__init__.py
@@ -1,8 +1,11 @@
 # A web server is a dockworker
-# it waits at a dock for HTTP letter-shipments to arrive
-# when they arrive it sends the raw data to the flask post office
-# then flask translates the parcels and sends them to the python backend
+# it waits at a dock for HTTP parcels to arrive
+# when they arrive, it checks them and passes them on to a messanger (WSGI)
+# the messanger carries them to the flask post office, part of a backend office
+# the messanger tells flask which room (function) it should go do, and flask sends it there
+# the workers (python logic) then handle the data
 
+# This file tells sys.path (directory of valid filepaths) that this folder is a package
 # This file (__init__.py) is necessary to tell sys.path (a directory of valid filepaths accessible by python)
 # that this folder is a package, that is, it contains importable modules
 # thus, it is a valid filepath
